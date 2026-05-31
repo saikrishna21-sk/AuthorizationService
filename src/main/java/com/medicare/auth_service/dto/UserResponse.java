e3700e12751ca1f7f5979e4fc27dac6a3e22d3ad
User <User@192.168.1.64>
2026-05-31 20:30:54 +0530

    user data commit

---
+++ src/main/java/com/medicare/auth_service/dto/UserResponse.java
@@ -0,0 +1,29 @@
+package com.medicare.auth_service.dto;
+
+public class UserResponse {
+
+	private Long id;
+	private String username;
+	private String role;
+
+	public UserResponse() {
+	}
+
+	public UserResponse(Long id, String username, String role) {
+		this.id = id;
+		this.username = username;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+}
